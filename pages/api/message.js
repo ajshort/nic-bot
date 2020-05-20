b1aa61912b5d8ea97d994f09089107b1b58f996d
Andrew Short <andrewjshort@gmail.com>
2020-05-20 10:48:23 +1000

    Allow posting VR vehicle messages to the VR ops channel

--- pages/api/message.js
+++ pages/api/message.js
@@ -3,19 +3,19 @@ const { channels } = require('./config');
 const { CardFactory } = require('botbuilder');
 const { ConnectorClient, MicrosoftAppCredentials } = require('botframework-connector');
 
-async function handleVrVehicleMessage(req, res, connector) {
+async function handleVrVehicleMessage(req, connector, channelId, channelName) {
   const { conversation, text, from } = req.body;
   const card = CardFactory.adaptiveCard(createVehicleMovementCard(from.name, text));
 
   await connector.conversations.createConversation({
     isGroup: true,
-    channelData: { channel: { id: channels.NIC.VR_OPERATORS } },
+    channelData: { channel: { id: channelId } },
     activity: { type: 'message', attachments: [card] },
   });
 
   await connector.conversations.sendToConversation(conversation.id, {
     type: 'message',
-    text: 'Thanks, I\'ll post this to the VR Operators channel.',
+    text: `Thanks, I'll post this to the ${channelName} channel.`,
   });
 }
 
@@ -41,9 +41,16 @@ export default async (req, res) => {
   const connector = new ConnectorClient(credentials, { baseUri: process.env.BOT_SERVICE_URL });
 
   // We assume that any messages tagging is in the WOL vehicle movements channels are a VR
-  // vehicle movement, so re-post it to the VR Operators channel.
-  if (recipient.name === 'VR' && channelData.teamsChannelId === channels.WOL.VEHICLE_MOVEMENTS) {
-    await handleVrVehicleMessage(req, res, connector);
+  // vehicle movement, so re-post it to the VR Operators channel. We do the same vice-versa
+  // for posts to the VR operators channel.
+  if (recipient.name === 'VR') {
+    if (channelData.teamsChannelId === channels.WOL.VEHICLE_MOVEMENTS) {
+      await handleVrVehicleMessage(req, connector, channels.NIC.VR_OPERATORS, 'VR Operators');
+    } else if (channelData.teamsChannelId === channels.NIC.VR_OPERATORS) {
+      await handleVrVehicleMessage(req, connector, channels.WOL.VEHICLE_MOVEMENTS, 'WOL Vehicle Movements');
+    } else if (channelData.teamsChannelId === channels.NIC.TESTBED) {
+      await handleVrVehicleMessage(req, connector, channels.NIC.TESTBED, 'Technology Testbed');
+    }
   }
 
   res.status(200).end();
